04a5f43fbac26b3f340dae93febc2e8ac9d6e2bc
Vencronys <150689455+vencronys@users.noreply.github.com>
2025-03-26 11:53:47 +0000

    Add my code for the Stack class (Pile)
    I do thinks my way, warry of any errors

--- cpp/tp7-ex3/src/main.cpp
+++ cpp/tp7-ex3/src/main.cpp
@@ -1,14 +1,28 @@
 #include "Stack.h"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
-  Stack stack(5);
-  stack.pop();
-  stack.add(13);
-  stack.add(7);
-  stack.print();
-  stack.print_size();
-  stack.print_capacity();
-  stack.pop();
-  stack.print();
+  Stack st1;
+  st1.print();
+  st1<3;
+  st1<13;
+  st1.print();
+  --st1;
+  st1.print();
+  Stack st2;
+  st2.print();
+  st2<3;
+  std::cout << ((st1 == st2) ? "Equal" : "Not Equal") << NEW_LINE;
+  st2<13;
+  std::cout << ((st1 != st2) ? "Equal" : "Not Equal") << NEW_LINE;
+  std::cout << st2[1] << NEW_LINE;
+  st2 = st2+5;
+  st2.print();
+  st1<1;
+  st2 = st2 + st1;
+  st2.print();
+  std::cout << st2 << NEW_LINE;
+  std::cin >> st2;
+  std::cout << st2 << NEW_LINE;
   return 0;
 }
